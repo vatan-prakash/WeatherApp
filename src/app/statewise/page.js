196191a4e499bbab4f1b659892ad8e494e1b6f03
Vatan Prakash <vatanprakash867@gmail.com>
2024-02-11 02:23:20 +0530

    first

--- src/app/statewise/page.js
+++ src/app/statewise/page.js
@@ -82,7 +82,7 @@ const StateWisePage = () => {
 
       {/* Render state-wise data */}
       <div className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">State-wise Data</h2>
+        {/* <h2 className="text-2xl font-bold mb-4">State-wise Data</h2> */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {stateWiseData.map((state, index) => (
             <div
